b9be59e03c6bbbd57e55830e764094c2dae133a5
Jeremy Osterhouse <jeremy@onetencommunications.com>
2020-01-07 11:39:38 -0500

    Add project files.

---
+++ CountYourChickens/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CountYourChickens
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
