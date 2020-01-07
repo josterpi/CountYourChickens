3d0a4d9601e9edcd21218c9e7a9612d14ca96044
Jeremy Osterhouse <josterhouse@gmail.com>
2020-01-07 12:16:54 -0500

    Pull in my code and update README

--- CountYourChickens/Program.cs
+++ CountYourChickens/Program.cs
@@ -1,12 +1,141 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace CountYourChickens
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public const int GAME_RUNS = 10000;
+        static void Main()
         {
-            Console.WriteLine("Hello World!");
+            var board = new Board();
+            var spinner = new Random();
+            var spinnerValues = Enum.GetValues(typeof(Character));
+            var results = new List<int>(GAME_RUNS);
+            for (var i = 0; i < results.Capacity; i++)
+            {
+                board = new Board();
+                while (true)
+                {
+                    var spin = (Character)spinnerValues.GetValue(spinner.Next(spinnerValues.Length));
+                    if (spin == Character.Blank) board.Fox();
+                    try
+                    {
+                        board.MoveTo(spin);
+                    }
+                    catch (GameOver)
+                    {
+                        break;
+                    }
+                }
+                results.Add(board.ChicksInCoop);
+            }
+
+            Console.WriteLine("Wins: {0}", results.Where(x => x >= 40).Count());
+            Console.WriteLine("Losses: {0}", results.Where(x => x < 40).Count());
+            Console.WriteLine("Average: {0}", results.Average());
+
+            Console.ReadKey();
         }
     }
+
+    public enum Character
+    {
+        Blank = 0, // Blank on the board, Fox on the spinner
+        Sheep,
+        Pig,
+        Tractor,
+        Cow,
+        Dog
+    }
+    public class Space
+    {
+        public Character Type { get; }
+        public bool Bonus { get; }
+        public bool LastSpace { get; }
+        public Space(Character character, bool bonus = false, bool last = false)
+        {
+            Type = character;
+            Bonus = bonus;
+            LastSpace = last;
+        }
+    }
+    public class Board
+    {
+        public List<Space> Spaces { get; }
+        public int ChickenPosition { get; set; } = 0;
+        public int ChicksInCoop { get; set; } = 0;
+        public Board()
+        {
+            Spaces = new List<Space>
+            {
+                new Space(Character.Blank), // Starting Space
+                new Space(Character.Blank),
+                new Space(Character.Sheep),
+                new Space(Character.Pig),
+                new Space(Character.Tractor, bonus: true),
+                new Space(Character.Cow),
+                new Space(Character.Dog),
+                new Space(Character.Pig),
+                new Space(Character.Cow, bonus: true),
+                new Space(Character.Dog),
+                new Space(Character.Sheep),
+                new Space(Character.Tractor),
+                new Space(Character.Blank),
+                new Space(Character.Cow),
+                new Space(Character.Pig),
+                new Space(Character.Blank),
+                new Space(Character.Blank),
+                new Space(Character.Blank),
+                new Space(Character.Tractor),
+                new Space(Character.Blank),
+                new Space(Character.Tractor),
+                new Space(Character.Dog),
+                new Space(Character.Sheep, bonus: true),
+                new Space(Character.Cow),
+                new Space(Character.Dog),
+                new Space(Character.Pig),
+                new Space(Character.Tractor),
+                new Space(Character.Blank),
+                new Space(Character.Sheep),
+                new Space(Character.Cow),
+                new Space(Character.Blank),
+                new Space(Character.Blank),
+                new Space(Character.Tractor),
+                new Space(Character.Pig),
+                new Space(Character.Sheep),
+                new Space(Character.Dog, bonus: true),
+                new Space(Character.Blank),
+                new Space(Character.Sheep),
+                new Space(Character.Cow),
+                new Space(Character.Pig, bonus: true),
+                new Space(Character.Blank, last: true)
+            };
+        }
+        public void Fox()
+        {
+            ChicksInCoop--;
+        }
+        public void MoveTo(Character character)
+        {
+            var moves = 1;
+            for (var i = ChickenPosition + 1; i < Spaces.Count; i++)
+            {
+                if (character == Spaces[i].Type || Spaces[i].LastSpace)
+                {
+                    break;
+                }
+                moves++;
+            }
+            ChickenPosition += moves;
+            ChicksInCoop += moves;
+            if (Spaces[ChickenPosition].Bonus) ChicksInCoop++;
+            if (ChickenPosition == Spaces.Count - 1) throw new GameOver();
+        }
+    }
+    public class GameOver : Exception
+    {
+    }
 }
+
--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # CountYourChickens
 Simulation of the children's cooperative board game
+
+This Christmas (2019), we got the board game [Count Your Chickens][1]. After a few plays and inspired by the memory of a fun write-up on a [simulation of Chutes & Ladders][2], I decided to program a quick simulation of Count Your Chickens to answer the question: what's the probability that you'll lose?
+
+The results of this simulation suggest that you'll actually lose about 40% of the time. On 10,000 runs, I count 6,002 wins and 3,998 loses. The mean number of chicks that you can get into the henhouse before the game ends is 39.84.
+
+And then Christmas break ended and I went back to work, so that's where this story ends.
+
+[1]: https://boardgamegeek.com/boardgame/94481/count-your-chickens
+[2]: https://jakevdp.github.io/blog/2017/12/18/simulating-chutes-and-ladders/
